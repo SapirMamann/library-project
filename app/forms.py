343c0af3ee1eb24a119b4f919542b3971dc565eb
Sapir <sapir1999@outlook.com>
2023-01-07 23:48:58 +0200

    adding customer views, model, and templates

--- app/forms.py
+++ app/forms.py
@@ -1,8 +1,12 @@
 from django.forms import ModelForm
-from .models import Book
+from .models import Book, Customer
 
 class BookForm(ModelForm):
-    
     class Meta:
         model = Book
+        fields = '__all__'
+
+class CustForm(ModelForm):
+    class Meta:
+        model = Customer
         fields = '__all__'
